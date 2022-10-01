ed305ba7fa68e35772e2ceaa7bb84ea702b8db2f
sm28797 <shubhanshu.mishra13@gmail.com>
2022-10-01 21:54:50 +0530

    Create SandglassStarPattern.java
    Adding this file to compete in cerner_2tothe5th_2022

---
+++ Day17/SandglassStarPattern.java
@@ -0,0 +1,37 @@
+// cerner_2^5_2022
+// cerner_2tothe5th_2022
+/* Program to print Sandglass pattern using * as shown below
+                        * * * * * 
+                         * * * * 
+                          * * * 
+                           * * 
+                            * 
+                           * * 
+                          * * * 
+                         * * * * 
+                        * * * * * 
+*/
+import java.util.Scanner;
+
+public class SandglassStarPattern {
+	public static void main(String[] args) {
+        int num;
+        Scanner s =new Scanner(System.in);
+        System.out.print("Enter the number of lines : ");
+        num=s.nextInt();
+        for(int i=1;i<=num;i++) {
+        	for(int j=1;j<=i;j++)
+        		System.out.print(" ");
+        	for(int j=num;j>=i;j--)
+        		System.out.print("* ");
+        	System.out.println();
+        }
+        for(int i=2;i<=num;i++) {
+        	for(int j=i;j<=num;j++)
+        		System.out.print(" ");
+        	for(int j=1;j<=i;j++)
+        		System.out.print("* ");
+        	System.out.println();
+        }
+	}
+}
