34f128fe283ee18eb3851e2d5af5005f2bf65d3e
sm28797 <shubhanshu.mishra13@gmail.com>
2022-10-11 22:38:48 +0530

    Create ReplaceSpaceInString.java

---
+++ Day27/ReplaceSpaceInString.java
@@ -0,0 +1,17 @@
+// cerner_2^5_2022
+// cerner_2tothe5th_2021
+// Program to replace Spaces in a string with *
+
+import java.util.Scanner;
+
+public class ReplaceSpaceInString {
+	public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.print("Enter the string to replace space with * : ");
+        String str=s.nextLine();
+        char ch;
+       	ch='*';
+       	str=str.replace(' ', ch);
+        System.out.print("String after space replacement is : "+str);
+	}
+}
